34a8c11c84ae62ea11895e9e9c90ce34a8b733b4
Maxime <maxime.amodei@gmail.com>
2021-04-13 23:13:30 +0200

    updated format wrt statement

--- lempelziv.py
+++ lempelziv.py
@@ -1,33 +1,44 @@
 "lempelziv: implements the online Lempel-Ziv compression algorithm"
 
-# TODO: what representation should the encoding have?
-
-from typing import Sequence, Tuple
+from typing import List, Sequence, Tuple
+import math
 
 class LempelZivDict:
+    """dictionary implementing fast access to both key and values
+    both keys and values must be unique"""
 
     def __init__(self) -> None:
-        self.store = dict()
-        self.words = []
-        self.max_value = 0
+        # base dict has the empty string at value 0
+        self.store = {'': 0}
+        self.words = ['']
+        self.max_value = 1
     
     def contains(self, key: str) -> bool:
+        "returns weither a certain key is present in the dictionary"
         return key in self.store
     
     def add(self, key: str) -> int:
+        "add a new word to the dictionary and returns its value"
         self.store[key] = self.max_value
         self.max_value += 1
         self.words.append(key)
         return self.max_value - 1
     
     def get_value(self, key: str) -> int:
+        "get the value of a word in the dictionary"
         return self.store[key]
 
     def get_word(self, value: int) -> str:
+        "get the word of a value in the dictionary"
         return self.words[value]
 
-def encode(sequence: str) -> Tuple[Sequence[int], LempelZivDict]:
-    """encode
+def encode(sequence: str) -> Tuple[List[str], LempelZivDict]:
+    """encode:
+        - create a sequence encoding the sequence using the LempelZiv algorithm
+    
+    returns:
+        - an encoded sequence as a list of item
+        - a dictionary usable to make the decoding
     """
 
     mapping = LempelZivDict()
@@ -41,21 +52,43 @@ def encode(sequence: str) -> Tuple[Sequence[int], LempelZivDict]:
     for hi in range(len(sequence)+1):
         current = sequence[lo:hi]
 
-        # wait until the word is not in the mapping and add it
-        if not mapping.contains(current):
-            val = mapping.add(current)
-            encoded.append(val)
-            lo = hi
+        new_word = not mapping.contains(current)
 
-    # empty the buffer
-    val = mapping.get_value(sequence[lo:])
-    encoded.append(val)
+        # wait until the word is not in the mapping and add it
+        if new_word or hi == len(sequence):
+            prefix_addr = mapping.get_value(sequence[lo:(hi-1)])
+            if new_word:
+                current_addr = mapping.add(current)
+
+            if current_addr == 1:
+                # encode in 0 bits
+                encoded.append(current[-1])
+            else:
+                addr_len = math.ceil(math.log2(current_addr))
+                encoded.append(f"{prefix_addr:0{addr_len}b}{current[-1]}")
+
+            lo = hi
 
     return mapping, encoded
     
 
-def decode(mapping: LempelZivDict, encoded: Sequence[int]) -> str:
+def decode(mapping: LempelZivDict, encoded: Sequence[str]) -> str:
     """decode
+        - mapping: the dictionary that was returned with the encoding
+        - encoded: the list of encoded value from the LempelZiv algorithm
+
+    returns:
+        - the original sequence
     """
-    return ''.join(mapping.get_word(n) for n in encoded)
+
+    decoded = ''
+    for s in encoded:
+        # empty address means 0 which is mapped to the empty string
+        if len(s) == 1:
+            decoded += s
+            continue
+        # get the address of the prefix
+        val = int(s[:-1], 2)
+        decoded += mapping.get_word(val) + s[-1]
+    return decoded
 
--- lempelziv_test.py
+++ lempelziv_test.py
@@ -1,19 +1,24 @@
 import lempelziv
 
-def test():
+def match(sequence: str, expected):
+    _, encoded = lempelziv.encode(sequence)
+    assert(', '.join(encoded) == expected)
+
+def test(sequence: str):
     import sys
-    sequence = ''.join(open('genome.txt').read().split('\n'))
-    # sequence = "ABCD"
 
     mapping, encoded = lempelziv.encode(sequence)
     res = lempelziv.decode(mapping, encoded)
 
-    if sequence != res:
-        print(f'comparison:\n\t{sequence}\n\t{res}')
-    else:
-        source_size = sys.getsizeof(sequence)
-        encoded_size = sys.getsizeof(encoded) + sys.getsizeof(mapping)
-        print(f"total size ratio: {encoded_size}/{source_size} = {encoded_size/source_size}")
+    assert(sequence == res)
+
+    source_size = sys.getsizeof(sequence)
+    encoded_size = sys.getsizeof(encoded) + sys.getsizeof(mapping)
+    print(f"total size ratio: {encoded_size}/{source_size} = {encoded_size/source_size}")
+
+
 
 if __name__ == "__main__":
-    test()
+    test(''.join(open('genome.txt').read().split('\n')))
+    test('1011010100010')
+    match('1011010100010', '1, 00, 011, 101, 1000, 0100, 0010')
