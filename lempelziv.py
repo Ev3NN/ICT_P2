f59d97799af2693b2b27dd825a9143e7f62cac35
Maxime <maxime.amodei@gmail.com>
2021-05-02 18:20:12 +0200

    updated lempelziv

--- lempelziv.py
+++ lempelziv.py
@@ -1,8 +1,9 @@
 "lempelziv: implements the online Lempel-Ziv compression algorithm"
 
-from typing import Tuple, Dict
-import numpy as np
 import math
+from typing import Tuple, Dict
+
+import numpy as np
 
 
 def binary_repr(value: int, width: int) -> np.ndarray:
@@ -10,15 +11,58 @@ def binary_repr(value: int, width: int) -> np.ndarray:
     return np.unpackbits(np.array([value], dtype='>u8').view(np.uint8))[-width:]
 
 
-def from_bin(repr: np.ndarray) -> int:
-    packed = np.packbits(repr)
-    return sum(print(v * (256) ** (len(packed) - i - 1)) for i, v in enumerate(packed))
+def from_bin(bits: np.ndarray) -> int:
+    "returns the value of a binary representation"
+    factors = 2 ** np.arange(len(bits))[::-1]
+    return np.dot(bits, factors)
 
 
 def encode_uint(sequence: np.ndarray) -> Tuple[np.ndarray, Dict[bytes, int]]:
     """encode a sequence of uint8 (char), returns the encoded message and the dictionary
     """
     return encode_bits(np.unpackbits(sequence))
+
+
+def encode_str(msg: str) -> Tuple[str, Dict[str, int], int]:
+    """encode an ASCII str, returns the encoded message, the dictionay and the
+    size in bits"""
+
+    dictionary = {'': 0}
+
+    next_addr = 1
+
+    prefix_key = ''
+    key = ''
+    total_size = 0
+
+    encoded = ''
+
+    start = 0
+    for end in range(len(msg)+1):
+        key = msg[start:end]
+
+        new_word = key not in dictionary
+        if new_word or end == len(msg):
+            prefix_addr = dictionary[prefix_key]
+            if new_word:
+                dictionary[key] = next_addr
+
+            if next_addr != 1:
+                addr_len = math.ceil(math.log2(next_addr))
+                encoded += f'{prefix_addr:0{addr_len}b}'
+                total_size += addr_len
+
+            encoded += msg[end-1:end]
+            total_size += 8  # an ASCII character
+
+            next_addr += 1
+
+            start = end
+            prefix_key = ''
+        else:
+            prefix_key = key
+
+    return encoded, dictionary, total_size
 
 
 def encode_bits(sequence: np.ndarray) -> Tuple[np.ndarray, Dict[bytes, int]]:
@@ -33,13 +77,13 @@ def encode_bits(sequence: np.ndarray) -> Tuple[np.ndarray, Dict[bytes, int]]:
 
     parts = []
 
-    lo = 0
-    for hi in range(len(sequence)+1):
-        key = sequence[lo:hi].tobytes()
+    start = 0
+    for end in range(len(sequence)+1):
+        key = sequence[start:end].tobytes()
 
         new_word = key not in dictionary
 
-        if new_word or hi == len(sequence):
+        if new_word or end == len(sequence):
             prefix_addr = dictionary[prefix_key]
             if new_word:
                 dictionary[key] = next_addr
@@ -48,13 +92,75 @@ def encode_bits(sequence: np.ndarray) -> Tuple[np.ndarray, Dict[bytes, int]]:
             if next_addr != 1:
                 addr_len = math.ceil(math.log2(next_addr))
                 parts.append(binary_repr(prefix_addr, addr_len))
-            parts.append(sequence[hi-1:hi])
+            parts.append(sequence[end-1:end])
 
             next_addr += 1
 
-            lo = hi
+            start = end
             prefix_key = b''
         else:
             prefix_key = key
 
     return np.concatenate(parts), dictionary
+
+
+def decode_bits(encoded: np.ndarray) -> np.ndarray:
+    "given a compressed sequence returns the original bit sequence"
+
+    # reverse of Dict[bytes, int] can be list[bytes]
+    reverse_dict = [b'']
+    decoded = []
+
+    reverse_dict.append(encoded[:1].tobytes())
+    decoded.append(np.frombuffer(encoded[:1].tobytes(), dtype=np.uint8))
+
+    length = 1
+    count_bkp = 1
+
+    # 1* 1 bits address
+    # 2* 2 bits
+    # 4* 3 buts
+    # ...
+
+    start = 1
+    count = count_bkp
+    while start != len(encoded):
+        if count == 0:
+            count_bkp *= 2
+            count = count_bkp
+            length += 1
+
+        addr = from_bin(encoded[start:start+length])
+
+        start += length
+        symbol = encoded[start]
+        start += 1
+
+        word = np.frombuffer(reverse_dict[addr], dtype=np.uint8)
+        word = np.concatenate([word, np.array([symbol], dtype=np.uint8)])
+
+        decoded.append(word)
+        reverse_dict.append(word.tobytes())
+
+        count -= 1
+
+    return np.concatenate(decoded)
+
+
+def decode_uint(encoded: np.ndarray) -> np.ndarray:
+    "given a compressed sequence returns the original uint8 sequence"
+    return np.packbits(decode_bits(encoded))
+
+
+def compress_text_file(path):
+    "load an UTF-8 file and compress its content, prints the compression ratio"
+    with open(path) as file:
+        content = file.read()
+        _, _, total_size = encode_str(content)
+        print(f'compression ratio: {8*len(content)/total_size}')
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        compress_text_file(sys.argv[1])
--- lempelziv_test.py
+++ lempelziv_test.py
@@ -1,28 +1,109 @@
-import lempelziv
+"testfile for lempelziv implementation"
+
+import unittest as u
 import numpy as np
+import lempelziv as lz
+
+
+class TestLempelZiv(u.TestCase):
+    "handles some tests for the LempelZiv compression"
+
+    def test_bin_repr(self):
+        "tests lempelziv.binary_repr"
+
+        self.assertEqual(0, lz.binary_repr(0, 1)[0])
+        self.assertEqual(1, lz.binary_repr(1, 1)[0])
+
+        self.assertEqual(5, len(lz.binary_repr(0, 5)))
+        self.assertEqual(5, len(lz.binary_repr(1, 5)))
+
+    def test_random_bits_check(self):
+        "checks the decoder (binary)"
+
+        for _ in range(100):
+            size = np.random.randint(500, 600, size=(1,))
+            msg = (np.random.random(size) < 0.5).view(np.uint8)
+
+            encoded, _ = lz.encode_bits(msg)
+            decoded = lz.decode_bits(encoded)
+
+            self.assertFalse((msg != decoded).any())
+
+    def test_random_uint_check(self):
+        "checks the decoder (uint8)"
+
+        for _ in range(100):
+            size = np.random.randint(500, 600, size=(1,))
+            msg = np.random.randint(256, size=size, dtype=np.uint8)
+
+            encoded, _ = lz.encode_uint(msg)
+            decoded = lz.decode_uint(encoded)
+
+            self.assertFalse((msg != decoded).any())
+
+    def test_example(self):
+        "reproduces the theoretical example"
+        msg = np.array([1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0], dtype=np.uint8)
+        expected = np.array([1, 0, 0, 0, 1, 1, 1, 0, 1, 1,
+                             0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], dtype=np.uint8)
+        encoded, _ = lz.encode_bits(msg)
+
+        self.assertFalse((encoded != expected).any())
+
+    def compress_bin_genome(self):
+        "compress the genome and prints the compression ratio (about 2s)"
+
+        symbols = ['A', 'T', 'C', 'G']
+        genome_file = open('genome.txt')
+        genome_txt = ''.join(genome_file.read().split('\n'))
+        genome_file.close()
+        # 0,1,2,3
+        genome_int = np.array([symbols.index(a)
+                               for a in genome_txt], dtype=np.uint8)
+        # get the two lowest bits
+        genome_int_bits = np.unpackbits(genome_int)
+        genome_int_bit0 = genome_int_bits[7::8]
+        genome_int_bit1 = genome_int_bits[6::8]
+        # join them
+        genome_int_bit_stacked = np.stack([genome_int_bit1, genome_int_bit0])
+        genome_bits = genome_int_bit_stacked.transpose().reshape((-1,))
+
+        print(f'{len(genome_bits)=}   ')
+
+        genome_encoded, _ = lz.encode_bits(genome_bits)
+
+        print(f'{len(genome_encoded)=}')
+        print(f'compression ratio = {len(genome_bits)}/{len(genome_encoded)}')
+        print(f'                  = {len(genome_bits) / len(genome_encoded)}')
+        # 1917114 / 2095205 = 0.9150006801243792
+
+    def compress_uint_genome(self):
+        "compress the ASCII version of the genome (about 6s)"
+
+        genome_file = open('genome.txt')
+        genome_txt = ''.join(genome_file.read().split('\n'))
+        genome_file.close()
+        # 0,1,2,3
+        genome_int = np.frombuffer(genome_txt.encode(), dtype=np.uint8)
+        genome_encoded, _ = lz.encode_uint(genome_int)
+
+        print(f'compression ratio = {8*len(genome_int)}/{len(genome_encoded)}')
+        print(f'                  = {8*len(genome_int) / len(genome_encoded)}')
+        # 7668456 / 3243053 = 2.3645793022809065
+
+    def compress_ascii_genome(self):
+        "compress the ASCII version of the genome (about 0.5s)"
+
+        genome_file = open('genome.txt')
+        genome_txt = ''.join(genome_file.read().split('\n'))
+        genome_file.close()
+
+        _, _, bin_size = lz.encode_str(genome_txt)
+        print(f'compression ratio = {8*len(genome_txt)}/{bin_size}')
+        print(f'                  = {8*len(genome_txt) / bin_size}')
+        # compression ratio = 7668456 / 2697854
+        #                   = 2.8424280928471295
 
 
 if __name__ == "__main__":
-    msg, _ = lempelziv.encode_bits(np.array([1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0], dtype=np.uint8))
-    expected = np.array([1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], dtype=np.uint8)
-    
-    if (msg != expected).any():
-        print("failed!")
-    
-    #genome = np.fromfile('genome.txt', dtype=np.uint8)
-    # ''.join(open('genome.txt').read().split('\n'))
-    try:
-        genome_sl = np.fromfile('genome_sl.txt', dtype=np.uint8)
-    except FileNotFoundError as e:
-        fout = open('genome_sl.txt', 'w')
-        fout.write(''.join(open('genome.txt').read().split('\n')))
-        fout.close()
-        genome_sl = np.fromfile('genome_sl.txt', dtype=np.uint8)
-    
-    genome_sl_enc, _ = lempelziv.encode_uint(genome_sl)
-
-    print('compression ratio (ignoring non ACTG characters, input encoded with 8 bits per symbol)')
-    print(f'\t{len(genome_sl) * 8 / len(genome_sl_enc)}')
-    print(f'{len(genome_sl):7} = number of characters')
-    print(f'{len(genome_sl_enc):7} = number of encoded bits')
-
+    u.main()
